1b1fe8f6f2ff67b61b091f9e7e56a43a220b3927
nairaalhosini <96581428+nairaalhosini@users.noreply.github.com>
2026-05-19 22:50:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+tasks = []
 
-# بيانات بسيطة
-X = [
-    [25, 50000],
-    [30, 60000],
-    [45, 80000],
-    [35, 120000],
-    [22, 20000],
-    [40, 95000]
-]
+@app.post("/task")
+def x(a: dict):
+    a["id"] = len(tasks)
+    tasks.append(a)
+    return a
 
-# Labels
-y = [0, 0, 1, 1, 0, 1]
+@app.get("/tasks")
+def z():
+    return tasks
 
-# تقسيم الداتا
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# إنشاء الموديل
-model = DecisionTreeClassifier()
-
-# تدريب الموديل
-model.fit(X_train, y_train)
-
-# التوقع
-predictions = model.predict(X_test)
-
-# حساب الدقة
-accuracy = accuracy_score(y_test, predictions)
-
-print("Predictions:", predictions)
-print("Accuracy:", accuracy)
-
-# تجربة على بيانات جديدة
-new_data = [[28, 70000]]
-result = model.predict(new_data)
-
-print("Prediction for new data:", result)
+@app.put("/task/{id}")
+def y(id: int, body: dict):
+    for i in tasks:
+        if i["id"] == id:
+            i.update(body)
+            return i
